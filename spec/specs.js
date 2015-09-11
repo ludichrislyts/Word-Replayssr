a6deada0e72e72042d64a8a41679caf20c1d37fd
JackMackerel <ludichrislyts@gmail.com>
2015-09-11 10:48:02 -0700

    also handle puctuation

--- spec/specs.js
+++ spec/specs.js
@@ -14,5 +14,10 @@ describe("findAndReplace", function(){
 	it("Takes a multiple phrase and finds and replaces the duplicate words, case insensitive", function(){
 		expect(findAndReplace("Super happy Slap Happy Pappy loves the happy flappy clappy clowns", "Happy", "pappy")).to.equal("Super pappy Slap pappy Pappy loves the pappy flappy clappy clowns")
 	});	
+	
+	it("Takes a multiple phrase with symbols and still finds and replaces the duplicate words, case insensitive", function(){
+		expect(findAndReplace("\"Super-happy\" Slap Happy Pappy loves the happy, flappy clappy clowns", "Happy", "pappy")).to.equal
+							 ("\"Super-pappy\" Slap pappy Pappy loves the pappy, flappy clappy clowns")
+	});
 
 });

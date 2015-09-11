5958d03a77f1d984c33bfc51e3d05a5fe4dda44a
JackMackerel <ludichrislyts@gmail.com>
2015-09-11 10:43:36 -0700

    get script to handle a phrase

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,17 @@ describe("findAndReplace", function(){
 	it("Takes a one word input and finds and replaces the word", function(){
 		expect(findAndReplace("happy", "Happy", "pappy")).to.equal("pappy")
 	});
+	
+	it("Takes a two word input and finds and replaces one word", function(){
+		expect(findAndReplace("Happy pappy", "happy", "pappy")).to.equal("pappy pappy")
+	});
+	
+	it("Takes a multiple word input with duplicate words and finds and replaces the duplicate word", function(){
+		expect(findAndReplace("happy happy pappy", "Happy", "pappy")).to.equal("pappy pappy pappy")
+	});
+
+	it("Takes a multiple phrase and finds and replaces the duplicate words, case insensitive", function(){
+		expect(findAndReplace("Super happy Slap Happy Pappy loves the happy flappy clappy clowns", "Happy", "pappy")).to.equal("Super pappy Slap pappy Pappy loves the pappy flappy clappy clowns")
+	});	
+
 });
